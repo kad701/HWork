5cf0c6ca147125d2d6459936bc0229dae14d35a5
Admin <kaWork89@gmail.com>
2025-08-20 22:40:18 +0300

    version 2.1 commit

--- src/javacore/basic/Basic.java
+++ src/javacore/basic/Basic.java
@@ -11,7 +11,7 @@ public class Basic {
     // Массив для хранения номеров телефонов контактов
     private static final String[] phoneNumbers = new String[MAX_CONTACTS];
     //текущее количесто записей справочнике
-    private static int currentAmount=0;
+    private static int currentAmount = 0;
 
     public static void main(String[] args) {
         Basic basic = new Basic();
@@ -29,45 +29,44 @@ public class Basic {
         names[5] = "shaun";
         phoneNumbers[5] = "245-11-29";
         for (int i = 0; i < names.length; i++) {
-            if(names[i]!=null)
+            if (names[i] != null)
                 currentAmount++;
         }
 
-        System.out.println(currentAmount);
         Scanner scanner = new Scanner(System.in);
         String option;
         //Основное меню
-        do{
+        do {
             System.out.println("МЕНЮ");
             System.out.println("1. Добавить контакт");
             System.out.println("2. Просмотреть контакты");
             System.out.println("3. Найти контакт");
             System.out.println("4. Удалить контакт ");
             System.out.println("5. Выйти");
-            option =scanner.nextLine();
+            option = scanner.nextLine();
             System.out.println("-------------");
-            System.out.println("Вы выбрали пункт "+option+" ");
-            if(option.equalsIgnoreCase("1")){
+            System.out.println("Вы выбрали пункт " + option + " ");
+            if (option.equalsIgnoreCase("1")) {
                 basic.addContacts(scanner);
-            continue;
+                continue;
             }
-            if(option.equalsIgnoreCase("2")){
+            if (option.equalsIgnoreCase("2")) {
                 basic.showContacts();
                 continue;
             }
-            if(option.equalsIgnoreCase("3")){
+            if (option.equalsIgnoreCase("3")) {
                 basic.findContacts(scanner);
-            continue;
-            }
-            if(option.equalsIgnoreCase("4")){
-                basic.deleteContact(scanner);
                 continue;
             }
-            else System.out.println("Выбранный вами пункт меню отсутствует, выберите еще раз");
+            if (option.equalsIgnoreCase("4")) {
+                basic.deleteContact(scanner);
+                continue;
+            } else System.out.println("Выбранный вами пункт меню отсутствует, выберите еще раз");
         }
         while (!option.equals("5"));
         System.out.println("Вы закончили работу");
     }
+
     // Реализация пунктов меню
     // ("20934".matches("-?[0-9]+"));
     public void addContacts(Scanner scanner) {
@@ -75,15 +74,15 @@ public class Basic {
         String name = scanner.nextLine();
         System.out.println("Введите имя телефон нового абонента ");
         String phoneNumber = scanner.nextLine();
-        if(phoneNumber.matches("-?[0-9]+")){
-        if((!name.equals(""))&&(!phoneNumber.equals(""))){
-            if (currentAmount < MAX_CONTACTS - 1) {
-                currentAmount = currentAmount + 1;
-                names[currentAmount - 1] = name;
-                phoneNumbers[currentAmount - 1] = phoneNumber;
-            } else System.out.println("Справочник заполнен, удалите контакт чтобы добавить новый");
-        } else System.out.println("Вы не ввели имя или номер абонента, новый абонент не добавлен");}
-        else System.out.println("Формат номера не верный,номер должен содержать только цифры, абонент не добавлен");
+        if (phoneNumber.matches("-?[0-9]+")) {
+            if ((!name.equals("")) && (!phoneNumber.equals(""))) {
+                if (currentAmount < MAX_CONTACTS - 1) {
+                    currentAmount = currentAmount + 1;
+                    names[currentAmount - 1] = name;
+                    phoneNumbers[currentAmount - 1] = phoneNumber;
+                } else System.out.println("Справочник заполнен, удалите контакт чтобы добавить новый");
+            } else System.out.println("Вы не ввели имя или номер абонента, новый абонент не добавлен");
+        } else System.out.println("Формат номера не верный,номер должен содержать только цифры, абонент не добавлен");
 
     } // добавить контакт
 
@@ -141,22 +140,20 @@ public class Basic {
                 names[i] = null;
                 phoneNumbers[i] = null;
                 currentAmount--;
-                System.out.println("Контакт "+name+" найден и удален");
-                for (int j = i; j < currentAmount+1; j++) {
+                System.out.println("Контакт " + name + " найден и удален");
+                for (int j = i; j < currentAmount + 1; j++) {
                     String tempName = names[j + 1];
-                    String tempPhoneNumber=phoneNumbers[j+1];
-                    if(tempName!=null){
+                    String tempPhoneNumber = phoneNumbers[j + 1];
+                    if (tempName != null) {
                         names[j] = tempName;
-                        phoneNumbers[j]=tempPhoneNumber;
-                    }
-                    else {
-                        names[j]=null;
-                        phoneNumbers[j]=null;
+                        phoneNumbers[j] = tempPhoneNumber;
+                    } else {
+                        names[j] = null;
+                        phoneNumbers[j] = null;
                     }
                 }
                 break;
-            }
-            else if(i==currentAmount-1)
+            } else if (i == currentAmount - 1)
                 System.out.println("контакт не найден");
         }
     }// удаление контакта по имени
